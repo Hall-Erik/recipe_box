6224c0aaddc82a00f93d971b46e2560093d3be8b
Erik <hall.erik@gmail.com>
2018-08-07 19:40:21 -0600

    Users can register, login and logout.

---
+++ recipebox/users/forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from recipebox.models import User
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Username',
+							validators=[DataRequired(), Length(max=20)])
+	email = StringField('Email',
+							validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password',
+							validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Sign Up')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user:
+			raise ValidationError('That username already exists. Please try another.')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user:
+			raise ValidationError('There is already a user associated with that email. Please try again.')
+
+class LoginForm(FlaskForm):
+	email = StringField('Email',
+							validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	remember = BooleanField('Remember Me')
+	submit = SubmitField('Sign Up')
