805f8a77b0c4bf78b614fc9b31c5c87c80cbf6e5
Erik <hall.erik@gmail.com>
2018-08-13 12:31:47 -0600

    user can delete recipes

--- recipebox/recipes/routes.py
+++ recipebox/recipes/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, flash, redirect, url_for
+from flask import (Blueprint, render_template, flash,
+					redirect, url_for, abort)
 from flask_login import current_user, login_required
 from recipebox import db
 from recipebox.recipes.forms import CreateRecipeForm
@@ -13,24 +14,19 @@ def create_recipe():
 	form = CreateRecipeForm()
 	if form.validate_on_submit():
 		recipe = Recipe(title=form.title.data, description=form.description.data, user_id=current_user.id)
-		
 		if form.picture.data:
 			picture = save_picture(form.picture.data)
 			recipe.image_file = picture
-		
 		db.session.add(recipe)
 		db.session.commit()
-
 		for ingredient in form.ingredients:
 			i = Ingredient(content=ingredient.data, recipe=recipe)
 			db.session.add(i)
 			db.session.commit()
-
 		for direction in form.directions:
 			d = Direction(content=direction.data, recipe=recipe)
 			db.session.add(d)
 			db.session.commit()
-		
 		flash('Your recipe has been added!', 'success')
 		return redirect(url_for('main.home'))
 	print(form.errors)
@@ -40,3 +36,14 @@ def create_recipe():
 def recipe(recipe_id):
 	recipe = Recipe.query.get_or_404(recipe_id)
 	return render_template('recipes/recipe.html', title=recipe.title, recipe=recipe)
+
+@recipes.route('/recipe/<int:recipe_id>/delete', methods=['POST'])
+@login_required
+def delete_recipe(recipe_id):
+	recipe = Recipe.query.get_or_404(recipe_id)
+	if recipe.author != current_user:
+		abort(403)
+	db.session.delete(recipe)
+	db.session.commit()
+	flash('Your recipe has been deleted!', 'success')
+	return redirect(url_for('main.home'))
