9e6cd3b6dfadf4f4b37c4088effe598b9632ae48
Erik <hall.erik@gmail.com>
2018-08-20 17:11:42 -0600

    bug fixes

--- recipebox/users/routes.py
+++ recipebox/users/routes.py
@@ -5,7 +5,7 @@ from recipebox.models import User
 from recipebox.users.forms import (RegistrationForm, LoginForm, 
 								UpdateAccountForm, UpdatePasswordForm,
 								RequestResetForm, ResetPasswordForm)
-from recipebox.users.utils import save_picture, send_reset_email
+from recipebox.users.utils import send_reset_email
 
 users = Blueprint('users', __name__)
 
