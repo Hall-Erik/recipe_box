25365b7623e99064fa6019fbad0c11ad11de118e
Erik <hall.erik@gmail.com>
2019-02-09 09:43:30 -0700

    don't even ask

--- recipebox/__init__.py
+++ recipebox/__init__.py
@@ -6,23 +6,33 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from recipebox.config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-mail = Mail(app)
 
-from recipebox.users.routes import users
-from recipebox.recipes.routes import recipes
-from recipebox.main.routes import main
-from recipebox.errors.handlers import errors
+mail = Mail()
 
-app.register_blueprint(users)
-app.register_blueprint(recipes)
-app.register_blueprint(main)
-app.register_blueprint(errors)
+def create_app(config_class=Config):
+	app = Flask(__name__)
+	app.config.from_object(Config)
+
+	db.init_app(app)
+	migrate.init_app(app, db=db)
+	bcrypt.init_app(app)
+	login_manager.init_app(app)
+	mail.init_app(app)
+
+	from recipebox.users.routes import users
+	from recipebox.recipes.routes import recipes
+	from recipebox.main.routes import main
+	from recipebox.errors.handlers import errors
+
+	app.register_blueprint(users)
+	app.register_blueprint(recipes)
+	app.register_blueprint(main)
+	app.register_blueprint(errors)
+
+	return app
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from recipebox import app
+from recipebox import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
